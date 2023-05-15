25c52a05169f1e01195cb663472cbea90162f820
Amr Salah <amr.salahml@gmail.com>
2023-05-16 00:05:16 +0200

    NewCode_12: histogram plotted

--- Assets/_UDVT/Scripts/Runtime/Visualization/VisHistogram.cs
+++ Assets/_UDVT/Scripts/Runtime/Visualization/VisHistogram.cs
@@ -8,7 +8,7 @@ public class VisHistogram : Vis
         title = "Histogram";
 
         //Define Data Mark and Tick Prefab
-        dataMarkPrefab = (GameObject)Resources.Load("Prefabs/DataVisPrefabs/Marks/Sphere");
+        dataMarkPrefab = (GameObject)Resources.Load("Prefabs/DataVisPrefabs/Marks/Cube");
         tickMarkPrefab = (GameObject)Resources.Load("Prefabs/DataVisPrefabs/VisContainer/Tick");
     }
 
@@ -17,6 +17,56 @@ public class VisHistogram : Vis
         base.CreateVis(container);
 
         //## 01:  Create Axes and Grids
+
+
+        /// Drow Plot but i didn't make the range of the drow true 
+
+        double[] Xarr = dataSets[0].ElementAt(0).Value;
+        int Xaxis = Xarr.Length;
+        double max = Xarr.Max();
+        double min = Xarr.Min();
+        Debug.Log(max);
+
+        Debug.Log(min);
+
+        int bin = 20
+
+        double binRange = max / bin;
+        Debug.Log(binRange);
+
+        double total1 = 0;
+
+        double[] X_arr = new double[bin];
+        double[] D_arr = new double[bin];
+
+        for (int i = 0; i < bin; i++)
+        {
+            D_arr[i] = 0;
+            X_arr[i] = i * binRange;
+
+        }
+
+        for (int i = 0; i < Xaxis; i++)
+        {
+            for (int j = 0; j < bin; j++)
+            {
+                if (((j - 1) * binRange) <= Xarr[i] && (j * binRange) > Xarr[i])
+                {
+                    D_arr[j]++;
+                    total1++;
+                    break;
+                }
+
+            }
+
+
+        }
+        double total = 0;
+        for (int i = 0; i < bin; i++)
+        {
+            i++;
+        }
+
 
         // X Axis
         visContainer.CreateAxis(dataSets[0].ElementAt(0).Key, dataSets[0].ElementAt(0).Value, Direction.X);
@@ -31,9 +81,13 @@ public class VisHistogram : Vis
         //visContainer.CreateGrid(Direction.Z, Direction.X);
 
         //## 02: Set Remaining Vis Channels (Color,...)
-        visContainer.SetChannel(VisChannel.XPos, dataSets[0].ElementAt(0).Value);
-        visContainer.SetChannel(VisChannel.YPos, dataSets[0].ElementAt(1).Value);
-        visContainer.SetChannel(VisChannel.ZPos, dataSets[0].ElementAt(2).Value);
+
+        visContainer.SetChannel(VisChannel.XPos, X_arr);
+        visContainer.SetChannel(VisChannel.YSize, D_arr);
+
+        //visContainer.SetChannel(VisChannel.XPos, dataSets[0].ElementAt(0).Value);
+        //visContainer.SetChannel(VisChannel.YSize, dataSets[0].ElementAt(1).Value);
+        // visContainer.SetChannel(VisChannel.ZPos, dataSets[0].ElementAt(2).Value);
         visContainer.SetChannel(VisChannel.Color, dataSets[0].ElementAt(3).Value);
 
         //## 03: Draw all Data Points with the provided Channels 
